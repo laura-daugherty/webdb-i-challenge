f931d0a3b3658f6b9ac16447b8f9cb8dd3dde382
Laura Daugherty <lauradaugherty6@gmail.com>
2019-08-12 20:45:12 -0400

    Endpoints -seeded knex -installed dependancies -made CRUD calls

--- server.js
+++ server.js
@@ -6,4 +6,51 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+  db('accounts')
+  .then(accounts => {
+    res.status(200).json(accounts)
+  })
+  .catch(err => {
+    res.status(500).json({error: "problem retrieving accounts"})
+  })
+})
+
+server.post('/', (req, res) => {
+  newAcct = req.body;
+  db('accounts')
+  .insert(newAcct, "id")
+  .then(response => {
+    res.status(200).json(response)
+  })
+  .catch(err => {
+    res.status(500).json({error: "problem"})
+  })
+})
+
+server.put('/:id', (req, res) => {
+  changes = req.body;
+  db('accounts')
+  .where('id', '=', req.params.id)
+  .update(changes)
+  .then(response => {
+    res.status(200).json(response)
+  })
+  .catch(err => {
+    res.status(500).json({error: "problem"})
+  })
+})
+
+server.delete('/:id', (req, res) => {
+  db('accounts')
+  .where('id', '=', req.params.id)
+  .del()
+  .then(response => {
+    res.status(200).json(response)
+  })
+  .catch(err => {
+    res.status(500).json({error: "problem"})
+  })
+})
+
 module.exports = server;
